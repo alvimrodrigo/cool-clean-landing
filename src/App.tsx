8e9f3131070044bc46491238a62e717f741a8f9d
Rodrigo Alvim <72860767+alvimrodrigo@users.noreply.github.com>
2026-04-07 23:44:45 -0300

    Set basename for BrowserRouter to '/cool-clean-landing'

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      <BrowserRouter basename="/cool-clean-landing">
     </TooltipProvider>
   </QueryClientProvider>
 );
